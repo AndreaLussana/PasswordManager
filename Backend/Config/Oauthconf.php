51a792158aa59fcd358d78cc1a32b43d780ef476
AndreaLussana <lussana.andrea03@gmail.com>
2022-05-12 18:28:16 +0200

    Remove the now ignored directory Backend

--- Backend/Config/Oauthconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-$client_id = "d1b64b1f083fda8008974799c795ba0f";
-$client_secret = "d54b1bd8921bd2a2bf9aaeebcb0741cd2ab7d2d50539daa4c6f89d5f319a433aa719c1caafc1065b9cec0e8e7272c3707ece1e01a137a2366c090476f2a91cf5";
-//$redirect = "http://localhost/PasswordManager/Backend/Oauth/redirect.php";
-$redirect = "http://lussana.altervista.org/PasswordManager/Backend/Oauth/redirect.php";
-?>
